8f8e0cbf017b27b91f182fb73b611094524720b8
DM <100419204+happydm09@users.noreply.github.com>
2023-08-05 11:08:22 +0900

    Update tag.js

--- js/tag.js
+++ js/tag.js
@@ -1,8 +1,26 @@
+var cur_tag = ''
+
 function SelectedTag(e) {
   var tag = e.getAttribute('tag')
-  var posts = document.getElementById('posts').querySelectorAll('[data-Jekyll]')
-
-  for (var i=0; i < posts.length; i++) {
-    console.log(posts[i])
+  var query = `[data-{tag}]`
+  var posts = document.getElementById('posts').querySelectorAll(query)
+  
+  if (cur_tag.includes(tag)) {
+    cur_tag = cur_tag.replace(tag, '')
+    console.log(tag)
+    console.log('이미 있음')
+    
+    for (var i=0; i < posts.length; i++) {
+      console.log(posts[i])
+    }
+    
+  } else {
+    
+    cur_tag += tag
+    console.log(tag)
+  
+    for (var i=0; i < posts.length; i++) {
+      console.log(posts[i])
+    }
   }
-} 
+}
