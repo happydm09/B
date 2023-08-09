9474449f191b7265f10dda0ae297fbcde2b0644a
DM <100419204+happydm09@users.noreply.github.com>
2023-08-09 21:56:23 +0900

    Update tag.js

--- js/tag.js
+++ js/tag.js
@@ -1,47 +1,8 @@
-var cur_tag = ''
+$("[data-tag]").click((e) => { filter(e.target.dataset.tag) })
 
-function SelectedTag(e) {
-  var tag = e.getAttribute('tag')
-  var query = `[data-${tag}]`
-  var posts = document.getElementById('posts').querySelectorAll(query)
-  
-  if (cur_tag.includes(tag)) {
-    e.className = e.className.replace(' selected', '')
-    cur_tag = cur_tag.replace(tag, '')
-    
-    for (var i=0; i < posts.length; i++) {
-      posts[i].className += ' hide'
-    }
-
-    if (cur_tag == '') {
-      Show_All_Posts()
-      alert('refresh')
-    }
-    
-  } else {
-    e.className += ' selected'
-    cur_tag += tag
-  
-    for (var i=0; i < posts.length; i++) {
-      posts[i].className = posts[i].className.replace(' hide', '')
-    }
-  }
+function filter(tag) {
+  $('.hidden').removeClass('hidden');
+  $('.post-wrapper').each((index, el) => {
+    if (!el.hasAttribute(`data-${tag}`)) { $(el).addClass('hidden') }
+  })
 }
-
-function Hide_All_Posts() {
-  var posts = document.getElementById('posts').querySelectorAll('div.container')
-  
-  for (var i=0; i < posts.length; i++) {
-      posts[i].className += ' hide'
-    }
-}
-
-function Show_All_Posts() {
-    var posts = document.getElementById('posts').querySelectorAll('div.container')
-  
-  for (var i=0; i < posts.length; i++) {
-      posts[i].className = posts[i].className.replace(' hide', '')
-    }
-}
-
-Hide_All_Posts()
