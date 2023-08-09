063e2ba48908e137ba02bcee0207c8beba4421e4
DM <100419204+happydm09@users.noreply.github.com>
2023-08-09 22:27:55 +0900

    Update tag.js

--- js/tag.js
+++ js/tag.js
@@ -1,8 +1,20 @@
-$("[data-tag]").click((e) => { filter(e.target.dataset.tag) })
+var tags = []
+
+$("[data-tag]").click((e) => {
+  var tag = e.target.dataset.tag
+  
+  if (tags.includes(tag)) { tags.splice(tag) }
+  else {
+    tags.push(tag)
+    filter(tags) 
+  }
+})
 
 function filter(tag) {
   $('.hide').removeClass('hide');
   $('.post').each((index, el) => {
-    if (!el.hasAttribute(`data-${tag}`)) { $(el).addClass('hide') }
+    for (var i=0; i < tags.lenght; i++) {
+      if (!el.hasAttribute(`data-${tag}`)) { $(el).addClass('hide') }
+    }
   })
 }
