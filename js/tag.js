8520e13fae149919ad2adda4913bbdf3216d9d14
DM <100419204+happydm09@users.noreply.github.com>
2023-08-09 22:03:44 +0900

    Update tag.js

--- js/tag.js
+++ js/tag.js
@@ -2,7 +2,7 @@ $("[data-tag]").click((e) => { filter(e.target.dataset.tag) })
 
 function filter(tag) {
   $('.hidden').removeClass('hidden');
-  $('.post-wrapper').each((index, el) => {
+  $('.post').each((index, el) => {
     if (!el.hasAttribute(`data-${tag}`)) { $(el).addClass('hidden') }
   })
 }
