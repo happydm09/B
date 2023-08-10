dd1632cbaa39711051e6399056ab4251614f4d26
DM <100419204+happydm09@users.noreply.github.com>
2023-08-10 12:54:44 +0900

    Update tag.js

--- js/tag.js
+++ js/tag.js
@@ -13,7 +13,7 @@ $("[data-tag]").click((e) => {
 function filter() {
   $('.hide').removeClass('hide');
   $('.post').each((index, el) => {
-    for (var i=0; i < tags.lenght; i++) {
+    for (var i=0; i < tags.length; i++) {
       console.log(tags[i])
       if (!el.hasAttribute(`data-${tags[i]}`)) { $(el).addClass('hide') }
       else {$(el).removeClass('hide')}
