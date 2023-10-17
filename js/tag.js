bf8aa24ff06aa4f1deba228469b653b446d3c3d2
DM <100419204+happydm09@users.noreply.github.com>
2023-10-17 21:38:37 +0900

    Update tag.js

--- js/tag.js
+++ js/tag.js
@@ -2,6 +2,29 @@
 
 var tags = []
 
+function filter() {
+  $('.hide').removeClass('hide')
+  $('.post').each((index, el) => {
+    var num = 0
+    for (var i=0; i < tags.length; i++) {
+      if (el.hasAttribute(`data-${tags[i]}`)) { num += 1 }
+    }
+    if (num == 0) { $(el).addClass('hide') }
+  })
+
+
+  if (tags.length == 0) { $('.hide').removeClass('hide') }
+}
+
+
+function SelectTags() {
+  $(`.selected`).removeClass('selected')
+  
+  for (var i=0; i < tags.length; i++) {
+      $(`.tag[data-tag=${tags[i]}]`).addClass('selected')
+    }
+}
+
 $("[data-tag]").click((e) => {
   var tag = e.target.dataset.tag
   
@@ -14,27 +37,6 @@ $("[data-tag]").click((e) => {
   filter()
   SelectTags()
 })
-
-function filter() {
-  $('.hide').removeClass('hide')
-  $('.post').each((index, el) => {
-    var num = 0
-    for (var i=0; i < tags.length; i++) {
-      if (el.hasAttribute(`data-${tags[i]}`)) { num += 1 }
-    }
-    if (num == 0) { $(el).addClass('hide') }
-  })
-
-  if (tags.length == 0) { $('.hide').removeClass('hide') }
-}
-
-function SelectTags() {
-  $(`.selected`).removeClass('selected')
-  
-  for (var i=0; i < tags.length; i++) {
-      $(`.tag[data-tag=${tags[i]}]`).addClass('selected')
-    }
-}
 
 // Search
 
