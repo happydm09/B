1616c198380ced184229018e6b365f5a049d8f4c
dongmin <100419204+happydm09@users.noreply.github.com>
2024-01-04 18:11:03 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,36 +3,3 @@ $(document).ready(function() {
   $('img').addClass('img-fluid');
   $('img').addClass('rounded');
 });
-
-function showCode(e) {
-  var target = document.getElementById(e.getAttribute('target'))
-  var parent = target.parentElement
-  
-  parent.querySelectorAll('.code').forEach(e => {
-    e.classList.add('hide')
-  })
-  
-  parent.querySelector('.cnav').querySelectorAll('.cfile').forEach(e => {
-    e.classList.remove('selcted')
-  })
-  
-  target.classList.remove('hide')
-  e.classList.add('selcted')
-}
-
-function init() {
-  var els = document.querySelectorAll('.cmain')
-  
-  els.forEach(el => {
-    var codes = el.querySelectorAll('.code')
-    
-    codes.forEach(e => {
-      console.log(e.getAttribute('main'))
-      if (!e.getAttribute('main')) {
-        e.classList.add('hide')
-      }
-    })
-  })
-}
-
-init()
